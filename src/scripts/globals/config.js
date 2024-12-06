a23885a69cab1f8af42854b6a4880e10aabd7f98
Bima Adityo Kurniawan <dito95.bluestars@gmail.com>
2024-12-06 08:10:19 +0700

    changed endpoint for backend to https://counter-begal-api.onrender.com, adjusted convertDate so it always return hh:mm

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: "http://localhost:3000",
+  BASE_URL: "https://counter-begal-api.onrender.com",
 };
 
 export default CONFIG;
--- src/scripts/utils/iso-date-converter.js
+++ src/scripts/utils/iso-date-converter.js
@@ -1,8 +1,8 @@
 function convertDate(isoDate) {
   const date = new Date(isoDate);
 
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
   const formattedTime = `${hours}:${minutes}`;
 
   const day = date.getDate();
